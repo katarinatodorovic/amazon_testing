842a0c16b1a0e4fed245120b7438d1066155a692
katarinatodorovic <kayattd@hotmail.com>
2025-11-28 01:34:46 +0100

    Add in reporter

--- utils/ReporterUtils.ts
+++ utils/ReporterUtils.ts
@@ -9,6 +9,11 @@ import { LoggerUtility } from "./LoggerUtility";
 
 class ReporterUtils implements Reporter {
 
+  private passed = 0;
+  private failed = 0;
+  private skipped = 0;
+  private timedOut = 0;
+
   onBegin(_config: FullConfig, suite: Suite) {
     LoggerUtility.info("--------------------------------------------------");
     LoggerUtility.info(`REPORTER: Test run started | Total tests: ${suite.allTests().length}`);
@@ -25,11 +30,17 @@ class ReporterUtils implements Reporter {
   onTestEnd(test: TestCase, result: TestResult) {
     const status = result.status.toUpperCase();
 
+    // Count test status
+    if (result.status === "passed") this.passed++;
+    else if (result.status === "failed") this.failed++;
+    else if (result.status === "skipped") this.skipped++;
+    else if (result.status === "timedOut") this.timedOut++;
+
     LoggerUtility.info("");
     LoggerUtility.info("========== REPORTER: TEST END ============");
     LoggerUtility.info(`Title : ${test.title}`);
     LoggerUtility.info(`Status: ${status}`);
-    
+
     if (result.error) {
       LoggerUtility.error(`Error: ${result.error.message}`);
     }
@@ -38,6 +49,14 @@ class ReporterUtils implements Reporter {
   }
 
   onEnd() {
+    LoggerUtility.info("--------------------------------------------------");
+    LoggerUtility.info("REPORTER SUMMARY");
+
+    LoggerUtility.info(`Passed : ${this.passed}`);
+    LoggerUtility.info(`Failed : ${this.failed}`);
+    LoggerUtility.info(`Skipped: ${this.skipped}`);
+    LoggerUtility.info(`Timed out: ${this.timedOut}`);
+
     LoggerUtility.info("--------------------------------------------------");
     LoggerUtility.info("REPORTER: Test run finished");
     LoggerUtility.info("--------------------------------------------------");
